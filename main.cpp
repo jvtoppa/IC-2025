f686077094e3f27f7c22f9e3304d593b719cd8f2
João Vitor Toppa <jvtoppa@gmail.com>
2025-05-03 18:48:01 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,47 +1,43 @@
-#include <iostream>
-#include "dataStructures.h"
+    #include <iostream>
+    #include "dataStructures.h"
+    #include <time.h>
 
-int main()
-{
-    PriorityQ pq(100); // 10 buckets (sqrt(100))
-
-    NODE* A = pq.addNode(10, 9);
-    //pq.incrementNodeOnBucket(&A);
-    NODE* B = pq.addNode(3, 3);
-    //pq.incrementNodeOnBucket(&B);
-    NODE* C = pq.addNode(10, 9);
-    //pq.incrementNodeOnBucket(&C);
-    NODE* D = pq.addNode(5, 5);
-    //pq.incrementNodeOnBucket(&D);
-    NODE* E = pq.addNode(200, 9);
-    //pq.incrementNodeOnBucket(&E);
-    pq.changeBucket(&E);
-    pq.changeBucket(&A);
-
-    std::cout << "Estado inicial:\n";
-    for(int i = 0; i < 10; i++)
+    int main()
     {
+        PriorityQ pq3(100);
 
-        pq.printLL(i);
-    }
+        for (int i = 0; i < 5; i++) {
+            int randomValue = std::rand() % 100000 + 10;
+            pq3.addNode(randomValue, 9);
+        }
     
-    //pq.changeBucket(&A);
-    //pq.changeBucket(&B);
-    //pq.changeBucket(&C);
-    //pq.changeBucket(&D);
-    //pq.changeBucket(&E);
-    //pq.incrementNodeOnBucket(&A);
-    //pq.incrementNodeOnBucket(&B);
-    //pq.incrementNodeOnBucket(&C);
-    //pq.incrementNodeOnBucket(&D);
-    //pq.incrementNodeOnBucket(&E);
+        pq3.printLL(9);  
 
 
-    std::cout << "Estado Final:\n";
-    for(int i = 0; i < 10; i++)
-    {
+        //----------------------------------x-------------------------------------------------//
+        std::string teste;
+        getline(std::cin, teste);
+        stringArray sa(teste.size(), teste);
+    
+        std::cout << "String original: " << sa.getString() << "\n";
+        std::cout << "Símbolos armazenados:\n";
+    
+        VECT** stringComplete = sa.getVectorizedString();
 
-        pq.printLL(i);
+        for (unsigned i = 0; i < teste.size(); i++) {
+            if(stringComplete[i]->previous == nullptr)
+            {
+                std::cout << "[-";
+            }
+            std::cout << stringComplete[i]->symbol;
+            if(stringComplete[i]->next == nullptr)
+            {
+                std::cout << "-] ";
+            }
+        }
+        std::cout << std::endl;
+    
+    return 0;
+
+    
     }
-return 0;
-}
